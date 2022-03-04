69ff0548de58d9b574d8bd9f187db7f38cd140c9
tarepan <tarepan5884@gmail.com>
2022-03-04 07:17:14 +0000

    Refactor duplicated non-SIMD sgemv_accum8x4

--- src/vec_avx.h
+++ src/vec_avx.h
@@ -679,8 +679,7 @@ typedef signed char qweight;
 #  define SCALE (128.f*127.f)
 #  define SCALE_1 (1.f/128.f/127.f)
 
-#if 1
-/* `DOT_PROD`, so maybe Int8 ops. */
+/* QGEMV (`DOT_PROD`, so maybe Int8 ops) */
 static inline void sgemv_accum8x4(float *_out, const qweight *w, int rows, int cols, int col_stride, const float *_x)
 {
    __m256i ones;
@@ -747,41 +746,8 @@ static inline void sgemv_accum8x4(float *_out, const qweight *w, int rows, int c
       _mm256_storeu_ps(&_out[i], vout);
    }
 }
-#else /* 1 */
-/* Not used implementation */
-static inline void sgemv_accum8x4(float *out, const qweight *w, int rows, int cols, int col_stride, const float *_x)
-{
-   int i, j;
-   unsigned char x[MAX_INPUTS];
-   (void)col_stride;
-   for (i=0;i<rows;i++) out[i] *= SCALE;
-   for (i=0;i<cols;i++) x[i] = 127+(int)floor(.5+127*_x[i]);
-   for (i=0;i<rows;i+=8)
-   {
-      for (j=0;j<cols;j+=4)
-      {
-         float *y;
-         float xj0, xj1, xj2, xj3;
-         xj0 = x[j+0];
-         xj1 = x[j+1];
-         xj2 = x[j+2];
-         xj3 = x[j+3];
-         y = &out[i];
-         y[0] += (w[0]*xj0+w[1]*xj1+w[2]*xj2+w[3]*xj3);
-         y[1] += (w[4]*xj0+w[5]*xj1+w[6]*xj2+w[7]*xj3);
-         y[2] += (w[8]*xj0+w[9]*xj1+w[10]*xj2+w[11]*xj3);
-         y[3] += (w[12]*xj0+w[13]*xj1+w[14]*xj2+w[15]*xj3);
-         y[4] += (w[16]*xj0+w[17]*xj1+w[18]*xj2+w[19]*xj3);
-         y[5] += (w[20]*xj0+w[21]*xj1+w[22]*xj2+w[23]*xj3);
-         y[6] += (w[24]*xj0+w[25]*xj1+w[26]*xj2+w[27]*xj3);
-         y[7] += (w[28]*xj0+w[29]*xj1+w[30]*xj2+w[31]*xj3);
-         w += 32;
-      }
-   }
-   for (i=0;i<rows;i++) out[i] *= SCALE_1;
-}
-#endif
 
+/* QSpMV */
 static inline void sparse_sgemv_accum8x4(float *_out, const qweight *w, int rows, int cols, const int *idx, const float *_x)
 {
    __m256i ones;
